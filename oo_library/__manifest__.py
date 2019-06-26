2cce2b3fc91e482e8641038af76a3ca766861f5a
Abdou <abdounasser202@gmail.com>
2019-06-26 12:49:45 +0100

    [IMP] Update oo_library

--- oo_library/__manifest__.py
+++ oo_library/__manifest__.py
@@ -1,41 +1,25 @@
 # coding:utf-8
 
 {
-    'name': 'Odoo for library',
-    'version': '0.1',
-    'category': 'Warehouse',
-    'description': """
-    Odoo for library
-
-    Odoo module for library management.
-
-    Features
-
-    - Consulting functions
-    - Acquisition functions (purchase suggestions, orders and receipt of
-    documents, pre-cataloging, and suppliers).
-    - The functions of management of printed periodicals: management of
-    subscriptions and states of collection, reception of numbers received;
-    this function is closely related to the acquisitions module for everything
-    related to subscription management.
-    - Bibliographic control functions: management of the description of the
-    collections (including the description of documents, copies, so-called
-    authorities entities, etc.), particularly by manual import or cataloging;
-    - Circulation functions: loan management, loan extensions, user bookings
-    and orders, delays (reminders, suspensions or fines for late readers), user
-    data
-    - Statistics functions: reporting on the use of the system and the data it
-    contains
-    """,
-    'author': 'Abdou Nasser',
-    'email': 'abdounasser202@gmail.com',
-    'website': 'https://www.nasserzone.com/',
+    'name': 'Odoo for library management',
+    "summary": "Odoo module for library management",
+    'category': 'Document Management',
+    # 'price': 35,
+    # 'currency': 'EUR',
+    "images": ['images/undraw_books_6rhq.png'],
+    'version': '0.0.2',
+    "author": "Abdou Nasser",
+    "support": "abdounasser202@gmail.com",
+    "website": "https://formation-odoo.blogspot.com",
+    "license": "LGPL-3",
     'depends': ['base', 'sale_management', 'purchase', 'stock'],
     'data': [
         'data/menu.xml',
-        'views/oo_consultation_view.xml' 
+        'views/oo_consultation_view.xml'
     ],
+    'demo': [],
+    'test': [],
     'installable': True,
-    'application': True,
     'auto_install': False,
+    'application': True,
 }
